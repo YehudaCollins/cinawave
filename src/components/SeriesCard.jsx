4b940d8640f6c2f3a41c70f2392b2d1e142a42d1
MeniViner <meniviner@gmail.com>
2024-07-16 15:27:10 +0300

    .

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { faHeart, faStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaHeart } from "react-icons/fa";
 import "../style/SeriesCard.css";
 
 function SeriesCard({ series }) {
@@ -14,29 +13,29 @@ function SeriesCard({ series }) {
         />
         <div className="overlay">
           <div className="series-info">
-            <p className="series-overview">{series.overview.slice(0, 100)}...</p>
-            <p className="series-seasons">Seasons: {series.number_of_seasons}</p>
+            <p className="series-overview">{series.overview ? series.overview.slice(0, 100) : 'No overview available'}...</p>
+            <p className="series-seasons">Seasons: {series.number_of_seasons || 'N/A'}</p>
           </div>
         </div>
       </div>
       <div className="likes">
-        <FontAwesomeIcon icon={faHeart} />
+        <FaHeart />
       </div>
       <div className="series-description">
         <div className="main-info-series">
           <div className="rating">
-            <FontAwesomeIcon icon={faStar} className="star-icon" />
-            <span>{series.vote_average.toFixed(1)}</span>
+            <FaHeart className="star-icon" />
+            <span>{series.vote_average ? series.vote_average.toFixed(1) : 'N/A'}</span>
           </div>
           <div className="year">
-            <span>{new Date(series.first_air_date).getFullYear()}</span>
+            <span>{series.first_air_date ? new Date(series.first_air_date).getFullYear() : 'N/A'}</span>
           </div>
         </div>
-        <h2 className="series-name">{series.name}</h2>
+        <h2 className="series-name">{series.name || 'No name available'}</h2>
         <div className="genres">
-          {series.genres.slice(0, 2).map((genre, index) => (
+          {series.genres ? series.genres.slice(0, 2).map((genre, index) => (
             <span key={index} className="genre-tag">{genre}</span>
-          ))}
+          )) : 'No genres available'}
         </div>
       </div>
     </div>
